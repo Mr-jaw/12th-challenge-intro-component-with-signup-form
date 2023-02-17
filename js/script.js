ccc57d9d7152e3bc27e0e62dc9669cbcb8f4a665
jaw_breaker <muhammadh1445@gmail.com>
2023-02-18 00:33:47 +0530

    accessibility update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 let input = document.getElementById("form").children
 
+const errormsg = [
+    "First name cannot be empty",
+    "Last name cannot be empty",
+    "Email cannot be empty",
+    "Password cannot be empty",
+]
 
 function Validation ()  {
     for (i=0; i <= 3; i++) {
@@ -10,7 +16,8 @@ function Validation ()  {
             input_field.style.background = "url('/images/icon-error.svg') no-repeat right"
             input_field.style.backgroundPositionX = '94%'
             input_field.style.borderColor = 'var(--red)'
-            emphasized_text.classList.remove('hidden')
+            emphasized_text.removeAttribute('hidden')
+            emphasized_text.innerText = errormsg[i]
             if (i === 2) {
                 emphasized_text.innerText = "Email cannot be empty"
                 input_field.style.color = 'var(--dark-blue)'
@@ -22,7 +29,7 @@ function Validation ()  {
 
         } else if (i === 2) {
             if (!input_field.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
-                emphasized_text.classList.remove('hidden')
+                emphasized_text.removeAttribute('hidden')
                 emphasized_text.innerText = "Looks like this not an email"
                 input_field.style.background = "url('/images/icon-error.svg') no-repeat right"
                 input_field.style.backgroundPositionX = '94%'
@@ -32,11 +39,11 @@ function Validation ()  {
                 input_field.style.background = "none"
                 input_field.style.borderColor = "var(--grayish-blue)"
                 input_field.style.color = 'var(--dark-blue)'
-                emphasized_text.classList.add('hidden')
+                emphasized_text.hidden = true;
             }
         } else if (i === 3) {
             if (input_field.value.length < 8) {
-                emphasized_text.classList.remove('hidden')
+                emphasized_text.removeAttribute('hidden')
                 emphasized_text.innerText = "Password must be atleast 8 characters long"
                 input_field.style.background = "url('/images/icon-error.svg') no-repeat right"
                 input_field.style.backgroundPositionX = '94%'
@@ -45,12 +52,13 @@ function Validation ()  {
                 input_field.style.background = "none"
                 input_field.style.borderColor = "var(--grayish-blue)"
                 input_field.style.color = 'var(--dark-blue)'
-                emphasized_text.classList.add('hidden')
+                emphasized_text.hidden = true;
             }
         } else {
+            console.log('log')
             input_field.style.background = "none"
             input_field.style.borderColor = "var(--grayish-blue)"
-            emphasized_text.classList.add('hidden')
+            emphasized_text.hidden = true;
         }
         
         
